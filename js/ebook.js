40e048f6539d2f9d46d73d57123012e272983c51
hyunsoo kim <ghgfhfg2@naver.com>
2022-05-06 17:57:34 +0900

    .

--- js/ebook.js
+++ js/ebook.js
@@ -1,15 +1,21 @@
 class Ebook {
   constructor(div,option){
     this.div = document.querySelector(div);
+    this.container = this.div.querySelector('.ebook__container');
     this.list = this.div.querySelectorAll('ul > li');
     this.length = this.list.length;
     this.page = 1;
     this.pageInput = this.div.querySelector(option.currentPage),
-    this.totalPage = this.div.querySelector('.ebook--page-total'),
-    this.btnMove = this.div.querySelector('.ebook--btn-page-move'),
+    this.totalPage = this.div.querySelector('.ebook__page-total'),
+    this.btnMove = this.div.querySelector('.ebook__btn-page-move'),
     this.pageShow = option.pageShow ? option.pageShow : 2;
-    this.prev = document.querySelector(option.prev);
-    this.next = document.querySelector(option.next);
+    this.prev = this.div.querySelector(option.prev);
+    this.next = this.div.querySelector(option.next);
+    this.breakPoints = option.breakPoints;
+    this.zoom = 1;
+    this.scaleText = this.div.querySelector('.ebook__scale-txt');
+    this.zoomIn = this.div.querySelector('.ebook__scale-zoom-in');
+    this.zoomOut = this.div.querySelector('.ebook__scale-zoom-out');
   }
   init(){
     this.prev.addEventListener('click',()=>{
@@ -18,10 +24,33 @@ class Ebook {
     this.next.addEventListener('click',()=>{
       this.toNext()
     })
-    this.btnMove.addEventListener('click',()=>{
-      this.toPage()
-    })
-
+    if(this.btnMove){
+      this.btnMove.addEventListener('click',()=>{
+        this.toPage()
+      })
+    }
+    if(this.breakPoints){
+      let winWidth = window.innerWidth;
+      let size = Object.keys(this.breakPoints).sort((a,b)=>b-a);
+      let curSize = size.filter(el=>el >= winWidth);
+      curSize = curSize.length > 0 ? Math.min.apply(null,curSize) : null;
+      if(curSize){
+        let option_ = this.breakPoints[curSize];
+        for(let key in option_){
+          this[key] = option_[key]
+        }
+      }
+    }
+    this.zoomIn.addEventListener('click',()=>{
+      this.imgZoomIn()
+    });
+    this.zoomOut.addEventListener('click',()=>{
+      this.imgZoomOut()
+    });
+    setTimeout(()=>{
+      let containerWidth = this.container.clientWidth;
+      this.div.querySelector('.ebook__page').style.width = containerWidth + 'px';
+    },100)
     this.pageSet();
   }
 
@@ -34,14 +63,14 @@ class Ebook {
     }
     if(this.pageShow === 2){      
       this.list[this.page-1].classList.add('active');
-      this.list[this.page] && this.list[this.page].classList.add('active');
+      this.list[this.page] && this.list[this.page].classList.add('active','border');
     }
-    this.totalPage.innerText = this.length
-    this.pageInput.value = this.page
+    if(this.pageInput){
+      this.totalPage.innerText = this.length
+      this.pageInput.value = this.page
+    }
   }
-  
   toPage(){
-
     let caclPage = parseInt(this.pageInput.value);
     if(caclPage > this.length){
       caclPage = this.length
@@ -50,24 +79,42 @@ class Ebook {
       caclPage = 1;
     }
     this.page = caclPage % 2 === 0 ? caclPage-1 : caclPage;
-
     this.pageSet()
   }
-
   toPrev(){
     if(this.page > 1){
       this.page -= this.pageShow;
       this.pageSet()
+    }else{
+      alert('첫번째 페이지 입니다.')
     }
   }
   toNext(){
-    if(this.page+this.pageShow < this.length){
+    if(this.page+this.pageShow <= this.length){
       this.page += this.pageShow;
       this.pageSet()
+    }else{
+      alert('마지막 페이지 입니다.')
     }
   }
 
-  getOption(){
-    console.log(this.option)
+  imgZoomIn(){
+    if(this.zoom < 3){
+      this.zoom += 0.2;
+      this.container.style = `--scale:${this.zoom}`;
+      this.scaleText.innerText = `${(this.zoom*100).toFixed(0)}%`;
+    }else{
+      alert('최대크기 입니다.')
+    }
   }
+  imgZoomOut(){
+    if(this.zoom > 0.6){
+      this.zoom -= 0.2;
+      this.container.style = `--scale:${this.zoom}`;
+      this.scaleText.innerText = `${(this.zoom*100).toFixed(0)}%`
+    }else{
+      alert('최소크기 입니다.')
+    }
+  }
+
 }
